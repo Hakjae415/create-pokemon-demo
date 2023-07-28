c6cfab160e49e45e80da3c085e026c3e1d111ace
Hakjae415 <hakjae.k415@gmail.com>
2023-07-28 11:48:53 -0400

    useState + useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 
 
 const App = () => {
-  
-  const getPokemon = async() => {
-    try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon")
-      const data = await response.json()
-      return data
-    } catch(err) {
-      console.error(err)
-    }
+  const [pokemonList, setPokemonList] = useState([])
 
-  }
+  useEffect(() => {
+    const getPokemon = async() => {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon")
+        const data = await response.json()
+        const first20Pokemon = data.results
+        setPokemonList(first20Pokemon)
+      } catch(err) {
+        console.error(err)
+      }
+    }
+    getPokemon()
+  },[])
 
   return (
     <>
@@ -24,11 +28,11 @@ const App = () => {
           <a href="https://facebook.com">FaceBook</a><br/>
           <a href="https://LinkedIn.com">LinkedIn</a>
         </nav>
-        <label>Username</label>
-        <input placeholder="Username"/><br/>
-        <label>Password</label>
-        <input placeholder="Password"/><br/>
-        <button>Submit</button>
+          <label>Username</label>
+            <input placeholder="Username"/><br/>
+          <label>Password</label>
+            <input placeholder="Password"/><br/>
+          <button>Submit</button>
       </header>
 
       <ul>
